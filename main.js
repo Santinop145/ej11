7f937ab3e07b10de8535113cf2bd9d978bcd36c2
santinop145 <santinoalessandrop@gmail.com>
2024-09-04 09:07:28 -0300

    Actualiza los archivos para añadir los botones de paginación

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const url = 'https://fakestoreapi.com/products';
+let photo = 0;
+let container = document.querySelector('.container');
+let products = [];
 
 function buildGallery(){
     fetch(url)
@@ -6,15 +9,32 @@ function buildGallery(){
         .then(data => {
             console.log(data);
             products = data;
-            let container = document.querySelector('.container');
-            for (const p of products){
-                let marco = document.createElement('div');
-                let image = document.createElement('img');
-                image.src = p.image;
-                marco.append(image);
-                container.append(marco);
-            }
+            showProducts();
     });
     let boton = document.getElementById('boton')
     boton.remove();
+}
+
+function showProducts(){
+    if(photo < 0){
+        photo = 0;
+    }
+    for (let i = 0; i < 4; i++){
+        console.log(photo);
+        let marco = document.createElement('div');
+        let image = document.createElement('img');
+        image.src = products[photo].image;
+        marco.append(image);
+        container.append(marco);
+        photo++;
+    }
+}
+
+function changePage(e){
+    console.log(e.target);
+    if(e.target.id === "previous"){
+        photo -= 8;
+    }
+    container.innerHTML = '';
+    showProducts();
 }
